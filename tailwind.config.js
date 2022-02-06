e97b2e1313e808ebfdc3f3468abfbdbaffe156d4
arogya <arogyabichpuria16@gmail.com>
2022-02-06 10:25:47 +0530

    tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#f7f4f4",
+        btnColor: "#3C5A5F",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
