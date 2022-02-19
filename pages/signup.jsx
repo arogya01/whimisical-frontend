a070c89120ad29ca3ab03048db7156906ee12cb1
arogya <arogyabichpuria16@gmail.com>
2022-02-19 15:53:32 +0530

    :zap: created the fetch functions for fetching the user info and storign it

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,7 +1,58 @@
+import Image from "next/image";
+import gradient from "../public/gradient03.jpg";
+import { useReducer, useEffect } from "react";
+
 export default function Signup() {
+  const initialState = {
+    name: "arogya",
+    email: "",
+    password: "",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "addUser":
+        return { ...state, [action.field]: action.payload };
+        break;
+    }
+  }
+
+  const [formState, dispatch] = useReducer(reducer, initialState);
+
+  const handleTextChange = (event) => {
+    dispatch({
+      type: "addUser",
+      field: event.target.name,
+      payload: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    console.log(formState);
+  }, [formState]);
+
+  const registerUser  = async (event) => {
+    event.preventDefault();
+    const res =await fetch("http://localhost:3001/signup", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method:"POST",
+      body:JSON.stringify(formState)
+    });
+
+    const result = await res.json(); 
+    console.log(result);
+  }
+
   return (
-    <div className="w-full h-screen m-auto flex flex-row items-center justify-center ">
-      <form action="POST" className="flex flex-row flex-wrap    border-2 p-6 w-1/2">
+    <div className="w-full h-screen flex flex-row  ">
+      <Image src={gradient} alt="something" width={500} height={500} />
+      <form
+        onSubmit={(event)=>{registerUser(event) }}
+        className="flex flex-row flex-wrap self-center mx-auto border-2 p-6 w-1/2"
+      >
         <div className="mb-4 w-full">
           <label htmlFor="full_name" className="block font-bold">
             Full Name
@@ -11,6 +62,10 @@ export default function Signup() {
             id="full_name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm block border-2   w-full  rounded-lg p-2.5"
             placeholder="arogya"
+            value={formState.name}
+            name="name" 
+            autoComplete="name"
+            onChange={(event) => handleTextChange(event)}
             required
           />
         </div>
@@ -21,7 +76,10 @@ export default function Signup() {
           <input
             type="email"
             id="email"
+            name="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm block border-2  w-full rounded-lg p-2.5"
+            value={formState.email}
+            onChange={(event) => handleTextChange(event)}
             placeholder="arogya@gmail.com"
             required
           />
@@ -33,15 +91,21 @@ export default function Signup() {
           <input
             type="password"
             id="password"
+            name="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm block border-2  w-full  rounded-lg p-2.5"
+            value={formState.password}
+            onChange={(event) => handleTextChange(event)}
             placeholder="arogya12"
             required
           />
         </div>
         <div className="mt-4 w-full grid place-content-center">
-        <button type="submit" className="bg-cyan-500 transition-all hover:bg-cyan-600  font-bold font-sans p-3 rounded-md mr-8 p-2.5">
-          Signup
-        </button>
+          <button
+            type="submit"
+            className="bg-cyan-500 transition-all hover:bg-cyan-600  font-bold font-sans p-3 rounded-md mr-8 p-2.5"
+          >
+            Signup
+          </button>
         </div>
       </form>
     </div>
