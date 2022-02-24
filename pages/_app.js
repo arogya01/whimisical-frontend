ac28a236793c74b236b15e5e20a69b88f908b6b4
arogya <arogyabichpuria16@gmail.com>
2022-02-24 22:40:40 +0530

    added authProvider in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import AuthProvider from '../context/authProvider'
+import GlobalSpinnerContextProvider from '../context/globalSpinnerContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GlobalSpinnerContextProvider>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+    </GlobalSpinnerContextProvider>
+  )
 }
 
 export default MyApp
