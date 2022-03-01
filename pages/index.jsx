66ab84dbf8bf940ff4a997bc1686cc019a828ce3
arogya <arogyabichpuria16@gmail.com>
2022-03-01 22:29:00 +0530

    added conifg files

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import BlogCard from "../components/blog-card";
 import BlogGrid from "../components/blog-grid";
 import Navbar from "../components/navbar";
 import {useState,useEffect} from "react";
