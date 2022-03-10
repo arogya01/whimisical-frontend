ff7c80145d1c741dfc82ccd67780994ef5b6e3f6
arogya <arogyabichpuria16@gmail.com>
2022-03-10 13:04:53 +0530

    minor changes

--- utility/popupDialog.jsx
+++ utility/popupDialog.jsx
@@ -1,13 +1,15 @@
-import Link from 'next/Link';
-
+import { useContext } from 'react';
+import { PopupDialogContext } from '../context/popupDialogContext';
 
 export default function PopupDialog(){
-    return( 
-        <div>
-        <div className="flex border-4 border-cyan-200 p-6">
-        <p>Signup Succssful</p>
-        <Link href="/login" />
+    const [isPopupDialogOn, setPopupDialog] = useContext(PopupDialogContext);
+
+    return(
+        isPopupDialogOn ? ( 
+        <div className="absolute flex border-4 border-cyan-200 p-6">
+        <p>Login Succssful</p>
         </div>
-        </div>
+
+        )  : ( " ")
     )
 }
