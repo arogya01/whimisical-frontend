d36c7988517506ca2552c2ad32a776afd486253d
arogya <arogyabichpuria16@gmail.com>
2022-03-10 13:02:15 +0530

    minor changes

--- context/authProvider.jsx
+++ context/authProvider.jsx
@@ -3,18 +3,8 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export default function AuthProvider(props) {
-//   const initialState = {
-//     full_name: "",
-//     email: "",
-//     token: "",
-//   };
   
   const [userAuth, setUserAuth] = useState({});
-   
-  // const retrieveUser = window.localStorage.getItem("userInfo");
-  // const result = retrieveUser.json();
-
-  // setUserAuth(result);
   return (
     <AuthContext.Provider value={[ userAuth, setUserAuth ]}>
       {props.children}
