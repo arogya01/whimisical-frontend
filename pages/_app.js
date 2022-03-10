0ab6f58d102cc2b8b197e37d14a547d47da54516
arogya <arogyabichpuria16@gmail.com>
2022-03-10 13:03:18 +0530

    added the popup context

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import '../styles/globals.css'
 import AuthProvider from '../context/authProvider'
 import GlobalSpinnerContextProvider from '../context/globalSpinnerContext'
-
+import PopupDialogContextProvider from '../context/popupDialogContext'
 function MyApp({ Component, pageProps }) {
   return (
+    <PopupDialogContextProvider>
     <GlobalSpinnerContextProvider>
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
     </GlobalSpinnerContextProvider>
+    </PopupDialogContextProvider>
   )
 }
 
