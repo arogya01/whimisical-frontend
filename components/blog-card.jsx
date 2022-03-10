4b45c90bd6539108ec1654dd3e4862414865164b
arogya <arogyabichpuria16@gmail.com>
2022-03-10 13:00:17 +0530

    :lipstick: removed the image import and changed the styling

--- components/blog-card.jsx
+++ components/blog-card.jsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import blogOne from "../public/4.jpg";
 
 export default function BlogCard({ imgSrc, blogHeadline, blogContent }) {
   return (
-    <div className="flex-col  font-sans border-2 w-80 m-4 ">
+    <div className="flex-col font-sans border-2 border-black rounded-lg w-80 m-4 ">
       <figure className="border-b-2">
         <Image src={imgSrc} alt="blog picture" width={300} height={180} />
       </figure>
