45bb2bea025d943d7dcaf695afba5526cc09a960
arogya <arogyabichpuria16@gmail.com>
2022-03-10 13:01:59 +0530

    made a few imports

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,14 +1,17 @@
 import { useReducer, useEffect, useContext } from "react";
-import {useRouter} from 'next/router';
-import Layout from '../components/Layout';
-import {AuthContext} from '../context/authProvider';
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+import { AuthContext } from "../context/authProvider";
 import { GlobalSpinnerContext } from "../context/globalSpinnerContext";
 import Spinner from "../utility/Spinner";
+import { PopupDialogContext } from "../context/popupDialogContext";
+import PopupDialog from "../utility/popupDialog";
 
 export default function Login() {
   const router = useRouter();
-  const [userAuth,setUserAuth] = useContext(AuthContext);
-  const [globalSpinner,setGlobalSpinner] = useContext(GlobalSpinnerContext);
+  const [userAuth, setUserAuth] = useContext(AuthContext);
+  const [globalSpinner, setGlobalSpinner] = useContext(GlobalSpinnerContext);
+  const [isPopupDialogOn, setPopupDialog] = useContext(PopupDialogContext);
 
   const initialState = {
     email: "",
@@ -33,20 +36,17 @@ export default function Login() {
 
   const [formState, dispatch] = useReducer(reducer, initialState);
 
-   useEffect(()=>{
-     console.log(formState);
+  useEffect(() => {
+    console.log(formState);
 
-     return(
-       setGlobalSpinner(false)
-     )
-   },[formState]);
+    return setGlobalSpinner(false);
+  }, [formState, setGlobalSpinner]);
 
   const handleUserLogin = async (event) => {
     setGlobalSpinner(true);
     event.preventDefault();
     console.log(formState);
-    try{
-
+    try {
       const res = await fetch("http://localhost:3001/login", {
         headers: {
           Accept: "application/json",
@@ -55,31 +55,25 @@ export default function Login() {
         method: "POST",
         body: JSON.stringify(formState),
       });
-    
+
       const userDetails = await res.json();
       console.log(userDetails);
       setUserAuth(userDetails);
-      localStorage.setItem("userInfo",JSON.stringify(userDetails));
+      setPopupDialog(true);
+      localStorage.setItem("userInfo", JSON.stringify(userDetails));
       router.push("/");
-
+    } catch (err) {
+      if (!err) {
+        console.log("no error response");
+      }
+      console.log("error message:", err);
+      setPopupDialog(true);
     }
-   catch(err){
-     if(!err){
-       console.log("no error response");
-     }
-     console.log("error message:",err);
-   }
   };
 
-  // useEffect(
-  //   ()=>{
-  //     router.push("/");
-  //   }
-  // ,[userAuth])
-
   return (
-    
     <div className="w-full h-screen m-auto flex flex-row items-center justify-center ">
+      <PopupDialog />
       <form
         onSubmit={(event) => {
           handleUserLogin(event);
@@ -98,7 +92,7 @@ export default function Login() {
             value={formState.email}
             placeholder="arogya@gmail.com"
             onChange={(event) => {
-             handleTextChange(event);
+              handleTextChange(event);
             }}
             required
           />
@@ -115,7 +109,7 @@ export default function Login() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm block border-2  w-full  rounded-lg p-2.5"
             placeholder="arogya12"
             onChange={(event) => {
-             handleTextChange(event);
+              handleTextChange(event);
             }}
             required
           />
@@ -123,19 +117,13 @@ export default function Login() {
         <div className="mt-4 w-full grid place-content-center">
           <button
             type="submit"
-            className="relative bg-cyan-500 transition-all hover:bg-cyan-600  font-bold font-sans p-3 rounded-md mr-8 p-2.5"
+            className="flex bg-cyan-500 transition-all hover:bg-cyan-600  font-bold font-sans p-3 rounded-md mr-8 p-2.5"
           >
-           {
-             globalSpinner ? (
-               <Spinner />
-             ) :
-             ""
-           }
+            {globalSpinner ? <Spinner /> : ""}
             Login
           </button>
         </div>
       </form>
     </div>
-    
   );
 }
